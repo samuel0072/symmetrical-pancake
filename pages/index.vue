a3bed441bb6f58716a6082234bcfa268a8372e3c
Samuel Lima da Silva <samuelima.00@outlook.com>
2022-06-27 10:11:02 -0300

    feat: Adiciona Dados de sorteios na página principal Adiciona link para a página de criar sorteio Adiciona links para páginas de detalhes de cada sorteio

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,40 @@
 <template>
   <div class="container">
-    <h1> Sorteios</h1>
+    <h1>Sorteios</h1>
+    <NuxtLink to="novoSorteio">
+      Novo Sorteio
+    </NuxtLink>
     <ul>
-
+        <li v-for="(sortition, index) in sortitions" :key="index">
+            {{sortition.nickname}}
+            <NuxtLink :to="'sorteio/' + sortition.id">
+                Visualizar
+            </NuxtLink>
+        </li>
     </ul>
   </div>
 </template>
 
 <script>
+const axios = require('axios');
+
 export default {
-  name: 'IndexPage',
-  data() {
-    return {
-    };
-  },
-  async asyncData() {
-
-  }
-
+    name: 'IndexPage',
+    data() {
+        return {
+            sortitions: []
+        };
+    },
+    mounted() {
+      this.getAllSortitions();
+    },
+    methods: {
+        getAllSortitions() {
+            axios.get("http://127.0.0.1:8002/api/sortition/all")
+                .then(res => {
+                    this.sortitions = res.data;
+                });
+        }
+    }
 }
 </script>
